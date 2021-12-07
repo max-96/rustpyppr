04ac242a8f92867cfeab40cde00c49e9712f5f19
Massimo Spaconi <massimo.spaconi@gmail.com>
2021-12-07 13:47:51 +0100

    improved example

--- examples/simple.py
+++ examples/simple.py
@@ -1,25 +1,27 @@
 import networkx as nx
 import rustpyppr
-# import random
+import random
 from pprint import pprint
 from itertools import chain
 import time
 
-# def create_random_graph(size):
-#     d = {i: [] for i in range(size)}
-#     for x in range(size):
-#         for y in random.sample(range(x+1, size), k=min(60, size-x-1)):
-#             d[x].append(y)
-#             d[y].append(x)
-#
-#     return d
+
+def create_random_graph(size):
+    d = {i: [] for i in range(size)}
+    for x in range(size):
+        for y in random.sample(range(x+1, size), k=min(60, size-x-1)):
+            d[x].append(y)
+            d[y].append(x)
+
+    return d
 
 
 def time_func(f, args, kwargs):
     t0 = time.perf_counter()
     res = f(*args, **kwargs)
     t1 = time.perf_counter()
-    print(f'>>> function {f.__name__} from {f.__module__} took {t1-t0:.6f} seconds')
+    print(
+        f'>>> function {f.__name__} from {f.__module__} took {t1-t0:.6f} seconds')
     return res
 
 
@@ -35,11 +37,12 @@ def compare_values(g, edge_dict):
     rust_res1 = rustpyppr.forward_push(edge_dict, 0, 0.85, 1e-6)
     rust_res2 = rustpyppr.forward_push_vec(edge_dict, 0, 0.85, 1e-6)
     rust_res3 = rustpyppr.forward_push_vec_lazy(edge_dict, 0, 0.85, 1e-6)
-    rust_res4 = rustpyppr.multiple_forward_push_vec(edge_dict, [0], 0.85, 1e-6)[0]
-    rust_res5 = rustpyppr.multiple_forward_push_vec_lazy(edge_dict, [0], 0.85, 1e-6)[0]
-    print_compare(nx_res, rust_res1, rust_res2, rust_res3, rust_res4, rust_res5)
-
-
+    rust_res4 = rustpyppr.multiple_forward_push_vec(
+        edge_dict, [0], 0.85, 1e-6)[0]
+    rust_res5 = rustpyppr.multiple_forward_push_vec_lazy(
+        edge_dict, [0], 0.85, 1e-6)[0]
+    print_compare(nx_res, rust_res1, rust_res2,
+                  rust_res3, rust_res4, rust_res5)
 
 
 def compare_time(g, edge_dict):
@@ -72,6 +75,16 @@ def test_caveman():
     compare_time(g, edge_dict)
 
 
+def test_my_random():
+    random.seed(0)
+    edge_dict = create_random_graph(20_000)
+    g = nx.from_dict_of_lists(edge_dict)
+    print(g)
+    compare_values(g, edge_dict)
+    compare_time(g, edge_dict)
+
+
 if __name__ == '__main__':
     test_complete_small()
     test_caveman()
+    test_my_random()

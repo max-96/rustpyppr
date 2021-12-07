f5d291efdf77f7f702e30a13dd8a682edd447878
Massimo Spaconi <massimo.spaconi@gmail.com>
2021-12-07 13:48:52 +0100

    updated signatures

--- rustpyppr.pyi
+++ rustpyppr.pyi
@@ -1,35 +1,35 @@
 from typing import Dict, List
 
 
-def forward_push_ppr(edge_dict: Dict[int, List[int]],
+def forward_push(edge_dict: Dict[int, List[int]],
                      source: int,
                      damping_factor: float,
                      r_max: float) -> Dict[int, float]:
     ...
 
 
-def forward_push_ppr_vec(edge_dict: Dict[int, List[int]],
+def forward_push_vec(edge_dict: Dict[int, List[int]],
                          source: int,
                          damping_factor: float,
                          r_max: float) -> Dict[int, float]:
     ...
 
 
-def forward_push_ppr_vec_lazy(edge_dict: Dict[int, List[int]],
+def forward_push_vec_lazy(edge_dict: Dict[int, List[int]],
                               source: int,
                               damping_factor: float,
                               r_max: float) -> Dict[int, float]:
     ...
 
 
-def multiple_forward_push_ppr_vec(edge_dict: Dict[int, List[int]],
+def multiple_forward_push_vec(edge_dict: Dict[int, List[int]],
                                   sources: List[int],
                                   damping_factor: float,
                                   r_max: float) -> Dict[int, Dict[int, float]]:
     ...
 
 
-def multiple_forward_push_ppr_vec_lazy(edge_dict: Dict[int, List[int]],
+def multiple_forward_push_vec_lazy(edge_dict: Dict[int, List[int]],
                                        sources: List[int],
                                        damping_factor: float,
                                        r_max: float) -> Dict[int, Dict[int, float]]:
